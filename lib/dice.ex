13c4f847fd4305d5c36c6aabcdd9eb4379885265
Ben Scherrey <scherrey@proteus-tech.com>
2016-06-23 11:40:33 +0700

    Add initial dice.

---
+++ lib/dice.ex
@@ -0,0 +1,17 @@
+defmodule Dice do
+
+    def d4, do: :random.uniform(4)
+
+    def d6, do: :random.uniform(6)
+
+    def d8, do: :random.uniform(8)
+
+    def d10, do: :random.uniform(10)
+
+    def d12, do: :random.uniform(12)
+
+    def d20, do: :random.uniform(20)
+
+    def d100, do: :random.uniform(100)
+
+end
