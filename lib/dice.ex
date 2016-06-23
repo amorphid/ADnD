c7619510be32a420931f0c939e349208d2f90537
Ben Scherrey <scherrey@proteus-tech.com>
2016-06-24 00:08:11 +0700

    Got the basic roll macro working finally!

--- lib/dice.ex
+++ lib/dice.ex
@@ -17,11 +17,13 @@ defmodule Dice do
     def threeD6, do: d6 + d6 + d6
 
     # roll d20 should display "Rolling #d20 : 17!" (where 17 will be whatever # is generated.)
-    defmacro roll(dice_expression) do
-        quote bind_quoted: [dice_expression: dice_expression] do
+    defmacro roll(orig_dice_expression) do
+        #quote bind_quoted: [dice_expression: dice_expression] do
+        quote do
+            dice_expression = unquote(orig_dice_expression)
             #IO.puts("Rolling #{String.to_atom(dice_expression)} : #{dice_expression}!")
             #IO.puts("Rolling #{inspect @dice_expression} : #{dice_expression}!")
-            IO.puts("Rolling #{Macro.escape(dice_expression)} : #{dice_expression}!")
+            IO.puts("Rolling #{unquote(Macro.to_string(orig_dice_expression))} : #{dice_expression}!")
             dice_expression
         end
     end
