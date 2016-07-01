efe2c69f0389d2ea4b417a5db18526f7fd546648
Michael Pope <mpope.cr@gmail.com>
2016-07-01 12:22:58 -0700

    Break out Character.Stats into its own module

--- lib/character.ex
+++ lib/character.ex
@@ -1,14 +1,11 @@
 defmodule Character do
     import Dice
-
-    defmodule Stats do
-        defstruct strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0, charisma: 0
-    end
+    alias Character.Stats
 
     defstruct name: "", stats: %Stats{}
 
     def roll_stats() do
-        %Stats{ strength: threeD6, intelligence: threeD6, wisdom: threeD6, 
+        %Stats{ strength: threeD6, intelligence: threeD6, wisdom: threeD6,
                 dexterity: threeD6, constitution: threeD6, charisma: threeD6 }
     end
 
@@ -18,7 +15,7 @@ defmodule Character do
     end
 
     def roll_stats(s = %Stats{}) do
-        r = Map.update!(s, :strength, fn 0 -> threeD6; value -> value end) 
+        r = Map.update!(s, :strength, fn 0 -> threeD6; value -> value end)
         r = Map.update!(r, :intelligence, fn 0 -> threeD6; value -> value end)
         r = Map.update!(r, :wisdom, fn 0 -> threeD6; value -> value end)
         r = Map.update!(r, :dexterity, fn 0 -> threeD6; value -> value end)
@@ -32,8 +29,8 @@ defimpl Inspect, for: Character.Stats do
     import Inspect.Algebra
 
     def inspect(item, options) do
-        msg = concat(["Str:", to_doc(item.strength, options), break, ", Int:", to_doc(item.intelligence, options), break, 
-                      ", Wis:", to_doc(item.wisdom, options), break, ", Dex:", to_doc(item.dexterity, options), break, 
+        msg = concat(["Str:", to_doc(item.strength, options), break, ", Int:", to_doc(item.intelligence, options), break,
+                      ", Wis:", to_doc(item.wisdom, options), break, ", Dex:", to_doc(item.dexterity, options), break,
                       ", Con:", to_doc(item.constitution, options), break, ", Chr:", to_doc(item.charisma, options) ])
         msg
     end
---
+++ lib/character/stats.ex
@@ -0,0 +1,3 @@
+defmodule Character.Stats do
+    defstruct strength: 0, intelligence: 0, wisdom: 0, dexterity: 0, constitution: 0, charisma: 0
+end
